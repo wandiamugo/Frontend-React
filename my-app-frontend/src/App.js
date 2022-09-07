4de2baa5e6f4ed1dc52a7dccbc443d4ff0376f29
Elizabeth Wandia Mugo <wwandia173@gmail.com>
2022-09-07 18:43:04 +0300

    app.css and app.js

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
+import React, {useState, useMemo, useContext} from "react";
 import './App.css';
+import NavBar from "./NavBar";
+import TodoApplication from './TodoApplication';
+import ShoppingApplication from "./ShoppingApplication";
+import Home from "./Home"
+import { Route, Switch } from "react-router-dom"
+
+import {UserContext} from './UserContext'
+
+
+
 
 function App() {
+
+
+  const [user, setUser] = useState({
+      username: "",
+      email_address: "",
+      id: "",
+      budget: ""
+    })
+  const providerValue = useMemo(()=>({user, setUser}), [user, setUser])
+
+  const [error, setError] = useState("")
+  const [errorMessage, setErrorMessage] = useState("")
+
+
+  async function login(details) {
+    await fetch("http://localhost:9292/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(details)
+    })
+      .then((r) => {
+        
+        if (r.status === 200) {
+          r.json().then((data) => 
+          setUser({
+            username: data.username,
+            email_address: data.email_address,
+            id: data.id,
+            budget: data.budget,
+            todos: data
+          })
+          
+      )} else {
+        setUser({
+          username: "",
+          email_address: "",
+          id: "",
+          budget: ""
+        })
+      }
+    }) 
+  }
+
+  function logout(){
+    setUser({
+      username: "",
+      email_address: ""});}
+
+     
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <UserContext.Provider value={providerValue}>
+  
+      <h1>List Metaverse</h1>
+      <NavBar />
+       <Switch>
+          <Route path="/todo-list">
+            <TodoApplication />
+          </Route>
+
+          <Route path="/shopping-list">
+            <ShoppingApplication/>
+          </Route>
+
+          <Route path="/">
+            <Home error={error} login={login} logout={logout}/>
+          </Route>
+      </Switch>
+
+    </UserContext.Provider>
     </div>
+    
   );
 }
 
